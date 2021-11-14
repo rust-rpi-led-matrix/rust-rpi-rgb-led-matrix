f29d129764b3b46d740ad728a8c2a8ed9f855d4a
Tyler Holmes <tyler@holmesengineering.com>
2021-11-13 18:07:30 -0800

    move the rust bindings to their own folder

--- rust-rpi-rgb-led-matrix/src/c.rs
+++ rust-rpi-rgb-led-matrix/src/c.rs
@@ -2,12 +2,6 @@ use crate::led_color::LedColor;
 use libc::{c_char, c_int};
 use std::ffi::CString;
 
-pub use crate::options::{LedMatrixOptions, LedRuntimeOptions};
-
-pub enum LedMatrix {}
-pub enum LedCanvas {}
-pub enum LedFont {}
-
 #[allow(dead_code)]
 impl LedCanvas {
     pub fn canvas_size(&self) -> (i32, i32) {
@@ -112,67 +106,4 @@ impl LedCanvas {
             }
         }
     }
-}
-
-#[link(name = "rgbmatrix")]
-extern "C" {
-    // unused C functions omitted
-    pub fn led_matrix_create_from_options_and_rt_options(
-        opts: *mut LedMatrixOptions,
-        rt_opts: *mut LedRuntimeOptions,
-    ) -> *mut LedMatrix;
-    pub fn led_matrix_delete(matrix: *mut LedMatrix);
-    pub fn led_matrix_get_canvas(matrix: *mut LedMatrix) -> *mut LedCanvas;
-    pub fn led_canvas_get_size(canvas: *const LedCanvas, width: *mut c_int, height: *mut c_int);
-    pub fn led_canvas_set_pixel(canvas: *mut LedCanvas, x: c_int, y: c_int, r: u8, g: u8, b: u8);
-    pub fn led_canvas_clear(canvas: *mut LedCanvas);
-    pub fn led_canvas_fill(canvas: *mut LedCanvas, r: u8, g: u8, b: u8);
-    pub fn led_matrix_create_offscreen_canvas(matrix: *mut LedMatrix) -> *mut LedCanvas;
-    pub fn led_matrix_swap_on_vsync(
-        matrix: *mut LedMatrix,
-        canvas: *mut LedCanvas,
-    ) -> *mut LedCanvas;
-    pub fn load_font(bdf_font_file: *const c_char) -> *mut LedFont;
-    pub fn delete_font(font: *mut LedFont);
-    pub fn draw_text(
-        canvas: *mut LedCanvas,
-        font: *const LedFont,
-        x: c_int,
-        y: c_int,
-        r: u8,
-        g: u8,
-        b: u8,
-        utf8_text: *const c_char,
-        kerning_offset: c_int,
-    ) -> c_int;
-    pub fn vertical_draw_text(
-        canvas: *mut LedCanvas,
-        font: *const LedFont,
-        x: c_int,
-        y: c_int,
-        r: u8,
-        g: u8,
-        b: u8,
-        utf8_text: *const c_char,
-        kerning_offset: c_int,
-    ) -> c_int;
-    pub fn draw_circle(
-        canvas: *mut LedCanvas,
-        x: c_int,
-        y: c_int,
-        radius: c_int,
-        r: u8,
-        g: u8,
-        b: u8,
-    );
-    pub fn draw_line(
-        canvas: *mut LedCanvas,
-        x0: c_int,
-        y0: c_int,
-        x1: c_int,
-        y1: c_int,
-        r: u8,
-        g: u8,
-        b: u8,
-    );
 }
